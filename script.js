444b06e708e450b7175d8683d581364d6d74f28d
Isabela Santos <107409432+Isa-Sant@users.noreply.github.com>
2026-02-17 12:25:36 -0300

    Add files via upload
    localStorage
    querySelectorAll
    forEach
    addEventListener
    manipulação de classes

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+// DARK MODE com salvar
+
 function darkMode() {
-    document.body.classList.toggle("dark");
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+localStorage.setItem("tema","dark");
+}else{
+localStorage.setItem("tema","light");
 }
+
+}
+
+if(localStorage.getItem("tema")==="dark"){
+document.body.classList.add("dark");
+}
+
+
+// destacar página ativa
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+
+if(link.href === window.location.href){
+link.style.borderBottom="2px solid white";
+}
+
+});
+
+
+// efeito clique botão
+
+const botoes = document.querySelectorAll(".btn");
+
+botoes.forEach(btn => {
+
+btn.addEventListener("click", () => {
+
+btn.style.transform="scale(0.95)";
+
+setTimeout(()=>{
+
+btn.style.transform="scale(1)";
+
+},150);
+
+});
+
+});
+
+
